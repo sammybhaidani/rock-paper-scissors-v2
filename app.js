f838f4ed1b1fd56c9ba19e9ddf02b1c22edccd93
Sammy <47990727+sammybhaidani@users.noreply.github.com>
2025-04-04 15:31:26 +0100

    Implement single round logic

--- app.js
+++ app.js
@@ -12,6 +12,38 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+
   const userChoice = prompt('Enter your choice: Rock, Paper or Scissors');
-  return userChoice;
+  const userChoiceFormatted = userChoice[0].toUpperCase() + userChoice.slice(1).toLowerCase();
+  
+  return userChoiceFormatted;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+  console.log(`You chose: ${humanChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+
+  if (humanChoice === computerChoice) {
+    console.log('Tie!');
+
+  } else if ((humanChoice === 'Rock' && computerChoice === 'Scissors') 
+    || (humanChoice === 'Paper' && computerChoice === 'Rock') 
+    || (humanChoice === 'Scissors' && computerChoice === 'Paper')) {
+
+    humanScore++;
+    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+
+  } else {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
