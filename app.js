a7f6d34166d83f723c99906f5d5d94dfe1580f2c
sammy <sammybhaidani@gmail.com>
2025-04-14 03:24:54 +0100

    Complete console version of rps following odin steps

--- app.js
+++ app.js
@@ -19,32 +19,34 @@ function getHumanChoice() {
   return userChoiceFormatted;
 }
 
-function playRound(humanChoice, computerChoice) {
-
-  console.log(`> You: ${humanChoice}`);
-  console.log(`> Computer: ${computerChoice}`);
-
-  if (humanChoice === computerChoice) {
-    console.log('Tie!');
-    return 'tie';
-
-  } else if ((humanChoice === 'Rock' && computerChoice === 'Scissors') 
-    || (humanChoice === 'Paper' && computerChoice === 'Rock') 
-    || (humanChoice === 'Scissors' && computerChoice === 'Paper')) {
-
-    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
-    return 'human';
-
-  } else {
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    return 'computer';
-  }
-}
-
 function playGame(rounds) {
 
   let humanScore = 0;
   let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+
+    console.log(`> You: ${humanChoice}`);
+    console.log(`> Computer: ${computerChoice}`);
+  
+    if (humanChoice === computerChoice) {
+      console.log('Tie!');
+      return 'tie';
+  
+    } else if ((humanChoice === 'Rock' && computerChoice === 'Scissors') 
+      || (humanChoice === 'Paper' && computerChoice === 'Rock') 
+      || (humanChoice === 'Scissors' && computerChoice === 'Paper')) {
+  
+      console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+      return 'human';
+  
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+      return 'computer';
+    }
+  }
 
   for (let round = 0; round < rounds; round++) {
 
@@ -63,6 +65,12 @@ function playGame(rounds) {
     console.log(`Current Score - You: ${humanScore} | Computer: ${computerScore}`);
   }
 
+  // playRound(getHumanChoice(), getComputerChoice());
+  // playRound(getHumanChoice(), getComputerChoice());
+  // playRound(getHumanChoice(), getComputerChoice());
+  // playRound(getHumanChoice(), getComputerChoice());
+  // playRound(getHumanChoice(), getComputerChoice());
+
   if (humanScore > computerScore) {
     console.log(`You won the game!`);
   } else if (computerScore > humanScore) {
@@ -73,5 +81,26 @@ function playGame(rounds) {
 
   console.log(`Final Score - You: ${humanScore} | Computer: ${computerScore}`);
 }
+
+// const rockButton = document.querySelector('.rock-button');
+// const paperButton = document.querySelector('.paper-button');
+// const scissorsButton = document.querySelector('.scissors-button');
+
+// rockButton.addEventListener('click', () => {
+//   // playRound('Rock', getComputerChoice());
+//   humanChoice = 'Rock';
+//   playGame();
+//   console.log('Rock button clicked!');
+// });
+
+// paperButton.addEventListener('click', () => {
+//   playRound('Paper', getComputerChoice());
+//   console.log('Paper button clicked!');
+// });
+
+// scissorsButton.addEventListener('click', () => {
+//   playRound('Scissors', getComputerChoice());
+//   console.log('Scissors button clicked!');
+// });
 
 playGame(5);
